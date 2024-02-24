e3ef1210ebcc318f67cc6ca22dbeaa7075189fcd
ibrahim <48119402+ibo-o@users.noreply.github.com>
2024-02-24 13:00:03 +0100

    Decompose All components + update

---
+++ DecomposeAllComponents.py
@@ -0,0 +1,10 @@
+def decompose_glyph_components(font):
+    for glyph in font:
+        if glyph.components:
+            glyph.decompose()
+
+font = CurrentFont()
+
+decompose_glyph_components(font)
+
+print("Done")
--- MakeMonoSpace.py
+++ MakeMonoSpace.py
@@ -1,14 +1,26 @@
 #Frederik Berlaen wrote it for me 24.01.20 on Discord DM
 
 monoWidth = 910
-for glyph in CurrentFont():
+
+font = CurrentFont()
+#this one only moves contours. Decomposing composites before running the script is recommended.
+for glyph in font:    
+    # calculate the diff in width and move only the contours
+    move = (monoWidth - glyph.width) / 2    
     glyph.width = monoWidth
+    for contour in glyph:
+       contour.moveBy((move, 0))
+       
+       
+#Old version
+# for glyph in CurrentFont():
+#     glyph.width = monoWidth
     
-    # Check if left and right margins are not None before performing operations
-    if glyph.leftMargin is not None and glyph.rightMargin is not None:
-        margins = glyph.leftMargin + glyph.rightMargin
-        glyph.leftMargin = margins / 2
-        glyph.rightMargin = margins / 2
+#     # Check if left and right margins are not None before performing operations
+#     if glyph.leftMargin is not None and glyph.rightMargin is not None:
+#         margins = glyph.leftMargin + glyph.rightMargin
+#         glyph.leftMargin = margins / 2
+#         glyph.rightMargin = margins / 2
     
-    # Reset it again to avoid rounding errors due to the left and right margin
-    glyph.width = monoWidth
+
+#     glyph.width = monoWidth
