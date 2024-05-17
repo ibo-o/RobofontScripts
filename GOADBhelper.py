19c7e74e104b197d5e50f40461aae0b33a035907
ibrahim <48119402+ibo-o@users.noreply.github.com>
2024-05-17 14:43:03 +0200

    Create GOADBhelper.py

---
+++ GOADBhelper.py
@@ -0,0 +1,26 @@
+f = CurrentFont()
+
+for gName in f.selectedGlyphNames:
+    glyph = f[gName]
+    print(f"{gName} {gName}")     
+
+
+
+# def printSelectedGlyphsUnicode():
+#     font = CurrentFont()
+#     if not font:
+#         print("No font open.")
+#         return
+    
+#     selectedGlyphs = [g for g in font.selectedGlyphNames]
+    
+
+#     for glyphName in selectedGlyphs:
+#         glyph = font[glyphName]
+#         if glyph.unicode:
+#             unicodeStr = f"uni{glyph.unicode:04X}"
+#             print(f"{glyphName} {glyphName} {unicodeStr}")
+#         else:
+#             print(f"{glyphName} {glyphName} #Has no Unicode value.")
+
+# printSelectedGlyphsUnicode()
