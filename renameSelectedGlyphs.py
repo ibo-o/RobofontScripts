141710fc6ee1d42ed807fc65b89d20533e09662d
ibrahim <48119402+ibo-o@users.noreply.github.com>
2024-05-17 14:00:09 +0200

    Create renameSelectedGlyphs.py

---
+++ renameSelectedGlyphs.py
@@ -0,0 +1,21 @@
+def renameSelectedGlyphs(oldSuffix, newSuffix):
+    font = CurrentFont()
+    if font is None:
+        print("No font open.")
+        return
+
+    selectedGlyphs = font.selectedGlyphNames
+    
+    for glyphName in selectedGlyphs:
+        if glyphName.endswith(oldSuffix):
+            newName = glyphName[:-len(oldSuffix)] + newSuffix
+            font.renameGlyph(glyphName, newName)
+            print(f"Renamed {glyphName} to {newName}")
+        else:
+            print(f"{glyphName} does not end with {oldSuffix}")
+
+
+oldSuffix = ".002"
+newSuffix = ".003" 
+
+renameSelectedGlyphs(oldSuffix, newSuffix)
