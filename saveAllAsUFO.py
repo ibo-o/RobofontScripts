89aedc33dd0bd002b76fd8a6cf617557a578a5e7
ibrahim <48119402+ibo-o@users.noreply.github.com>
2025-08-19 22:19:07 +0300

    Update saveAllAsUFO.py

--- saveAllAsUFO.py
+++ saveAllAsUFO.py
@@ -5,7 +5,7 @@ output_dir = "/Users/ibrahim/Desktop/ufo"
 os.makedirs(output_dir, exist_ok=True)
 
 for font in AllFonts():
-    font_name = font.info.postscriptFullName 
+    font_name = font.info.postscriptFontName 
     save_path = os.path.join(output_dir, f"{font_name}.ufo")
     font.save(save_path)
     print(f"Saved: {save_path}")
